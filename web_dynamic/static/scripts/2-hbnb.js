e0b11eed539948f2607849ad4a157fbc4839c4c5
Bard-Budist <op7puro@live.com>
2020-05-18 16:45:42 +0000

    Fix:Organize and clean Jquery

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,13 +1,15 @@
 /* executed only when DOM is loaded */
 $(document).ready(function () {
+  // Check status of API and show the status color
   $.getJSON('http://127.0.0.1:5001/api/v1/status/', function (data) {
     if (data.status === 'OK') {
       console.log(data.status);
       $('DIV#api_status').addClass('available');
-    } else {
-      $('DIV#api_status').removeClass('available');
     }
+  }).fail(function () {
+    $('DIV#api_status').removeClass('available');
   });
+  // Load amenities when click event
   $('input[type="checkbox"]').click(function () {
     let listaAmenity = [];
     if (!$('h4:eq(1)').is(':empty')) {
